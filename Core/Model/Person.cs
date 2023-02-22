65bfd85681bc0d00c07b781af12608dc0c89c7c9
CodenameClass <jens.ingels-business@outlook.com>
2023-02-22 22:26:55 +0100

    Implement Core

---
+++ Core/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class Person
+    {
+        public Guid Id { get; init; }
+        public string Firstname { get; init; }
+        public string Lastname { get; init; }
+        public List<string> SocialSkills { get; init; }
+        public Dictionary<string, string> SocialAccounts { get; init; }
+    }
+}
---
+++ Core/Repositories/IPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface IPersonRepository
+    {
+        Person GetPerson(Guid id);
+        List<Person> GetAllPeople();
+        void AddPerson(Person person);
+        void UpdatePerson(Person person);
+        void DeletePerson(Guid id);
+    }
+}
