52da22917bcb4f21f81c4c49d0f97196ce553d18
CodenameClass <jens.ingels-business@outlook.com>
2023-02-23 23:21:18 +0100

    Update Cui for debugging

--- Cui/Program.cs
+++ Cui/Program.cs
@@ -1,8 +1,131 @@
-﻿using System.Text.Json;
+﻿using Core.Model;
+using Core.Repositories;
+using DataAccessFile.Data;
+using DataAccessFile.Repositories;
+using System;
+using System.Text.Json;
 
-internal class Program
+
+public class Startup
 {
-    private static void Main(string[] args)
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await Task.Run(() =>
+            {
+                new Startup().RunApp();
+            });
+        }
+
+    }
+
+    public void RunApp()
+    {
+        Debug();
+        //MyOutput();
+    }
+
+    public void Debug()
+    {
+        DataContext dataContext = new DataContext("data.json");
+        IPersonRepository personRepo = new PersonFileRepository(dataContext);
+
+        var people = personRepo.GetAllPeopleAsync().Result;
+
+        Console.WriteLine($"\n-- should be empty open --");
+        people
+            .ForEach(person => {
+                Console.WriteLine($"{person.Id} {person.Firstname} {person.Lastname}");
+            });
+        Console.WriteLine($"-- should be empty end --\n"); ;
+
+
+        //DELETE
+        people
+            .ForEach(person => {
+                personRepo.DeletePerson(person.Id);
+            });
+
+        dataContext.SaveChangesAsync().Wait();
+
+        /*
+        Parallel.ForEachAsync(people, async (person, token) =>
+        {
+            await personRepo.DeletePersonAsync(person.Id);
+        });
+        */
+
+        //SHOW DELETE
+        Console.WriteLine($"\n-- should be empty open --");
+        people
+            .ForEach(person => {
+
+                Console.WriteLine($"{person.Firstname} {person.Lastname}");
+            });
+        Console.WriteLine($"-- should be empty end --\n");
+
+
+        //ADD
+        personRepo.AddPerson(new Person()
+        {
+            Id = Guid.NewGuid(),
+            Firstname = "John",
+            Lastname = "Doe",
+            SocialSkills = new List<string> { "social", "fun", "coach" },
+            SocialAccounts = new Dictionary<string, string>
+            {
+                {  "Twitter", "@JohnDoe" },
+                {  "Linkedin", "Linkedin.com/johndoe" }
+            }
+        });
+
+        dataContext.SaveChangesAsync().Wait();
+
+
+        //SHOW ADD
+        people = personRepo.GetAllPeopleAsync().Result;
+
+        Console.WriteLine($"\n-- should be empty open --");
+        people
+            .ForEach(person => {
+
+                Console.WriteLine($"{person.Firstname} {person.Lastname}");
+            });
+        Console.WriteLine($"-- should be empty end --\n");
+
+
+        //UPDATE
+        personRepo.UpdatePerson(new Person()
+        {
+            Id = personRepo.GetAllPeopleAsync().Result.First().Id,
+            Firstname = "John",
+            Lastname = "Boris",
+            SocialSkills = new List<string> { "social", "fun", "coach" },
+            SocialAccounts = new Dictionary<string, string>
+            {
+                {  "Twitter", "@JohnDoe" },
+                {  "Linkedin", "Linkedin.com/johndoe" }
+            }
+        });
+
+
+        //SHOW UPDATE
+        people = personRepo.GetAllPeopleAsync().Result;
+
+        Console.WriteLine($"\n-- should be empty open --");
+        people
+            .ForEach(person => {
+
+                Console.WriteLine($"{person.Firstname} {person.Lastname}");
+            });
+        Console.WriteLine($"-- should be empty end --\n");
+
+
+        Console.WriteLine($"-------------------------");
+    }
+
+    public void MyOutput()
     {
         PersonInputDto personInputDto = new PersonInputDto()
         {
@@ -25,16 +148,13 @@ internal class Program
         Console.WriteLine($"The reverse version of the firstname and lastname: {ReverseString(fullName)}");
         Console.WriteLine($"The JSON format of the entire object:\n{FormatDtoToJson(personInputDto)}");
     }
-
-
-    public static int NumberOfVowelsInString(string entry) => entry.Count(c => "aeiou".Contains(char.ToLower(c)));
-    public static int NumberOfConstenantsInString(string entry) => entry.Count(c => "bcdfghjklmnpqsrtvwxyz".Contains(char.ToLower(c)));
-    public static string ReverseString(string entry)
+    public int NumberOfVowelsInString(string entry) => entry.Count(c => "aeiou".Contains(char.ToLower(c)));
+    public int NumberOfConstenantsInString(string entry) => entry.Count(c => "bcdfghjklmnpqsrtvwxyz".Contains(char.ToLower(c)));
+    public string ReverseString(string entry)
     {
         char[] charArray = entry.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
     }
-    public static string FormatDtoToJson(object inputDto) => JsonSerializer.Serialize(inputDto, new JsonSerializerOptions { WriteIndented = true });
-
+    public string FormatDtoToJson(object inputDto) => JsonSerializer.Serialize(inputDto, new JsonSerializerOptions { WriteIndented = true });
 }
